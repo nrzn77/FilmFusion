5b271c9787e50a56194ea819af017ec93589b172
fardeensadab <fardeensadab@gmail.com>
2024-11-07 17:55:16 +0600

    Database with react app

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GenreSelector from "./component/GenreSelector";
+import MovieList from "./component/MovieList";
+import "./App.css";
+
+function App() {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/genres") // assuming we have an endpoint for genres
+      .then((response) => setGenres(response.data))
+      .catch((error) => console.error("Error fetching genres:", error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedGenre) {
+      axios
+        .get(`http://localhost:3001/movies/${selectedGenre}`)
+        .then((response) => setMovies(response.data))
+        .catch((error) => console.error("Error fetching movies:", error));
+    }
+  }, [selectedGenre]);
+
+  return (
+    <div className="App">
+      <h1>Movie Browser</h1>
+      <GenreSelector genres={genres} onSelectGenre={setSelectedGenre} />
+      {selectedGenre && <MovieList movies={movies} />}
+    </div>
+  );
+}
+
+export default App;
